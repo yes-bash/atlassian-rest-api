183dcdd42cbead69e3450bea9b343c1f76f92572
yes-bash <123662544+yes-bash@users.noreply.github.com>
2024-08-09 19:09:25 +0200

    Create confluence_add-admin-to-restricted-page.sh

---
+++ confluence_add-admin-to-restricted-page.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. .env
+
+pageId="${1}"
+
+# save old restrictions
+curl -s --netrc-file ${netrcFileConfluence} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
+        -b "confluence.asSuperAdmin=true" \
+        --url "${url}/wiki/rest/api/content/${pageId}/restriction" > ${pageId}.restictions
+
+
+# overwrite permissions with our admin user
+curl -s --netrc-file ${netrcFileConfluence} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
+        --url "${url}/cgraphql?q=DirectRestrictionsMutation" \
+        -o /dev/null \
+        -X POST \
+        --data '[{"operationName":"DirectRestrictionsMutation","variables":{"pageId":"'${pageId}'","restrictions":{"read":{"group":[],"user":[{"id":"'${confluenceUserId}'"}]},"update":{"group":[],"user":[{"id":"'${confluenceUserId}'"}]}}},"query":"mutation DirectRestrictionsMutation($pageId: ID\u0021, $restrictions: PageRestrictionsInput) @asSuperAdmin {\n  updatePage(input: {pageId: $pageId, restrictions: $restrictions}) {\n    page {\n      id\n      __typename\n    }\n    __typename\n  }\n}\n"}]'
