fcd98d1d432ba7ca86b29820fb472f98372dc8a1
yes-bash <123662544+yes-bash@users.noreply.github.com>
2024-08-09 18:55:25 +0200

    Create enable-super-admin.sh

---
+++ enable-super-admin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. .env
+
+curl -s --netrc-file ${netrcFile} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
+        --url "${url}/cgraphql?q=EnableSuperAdmin" \
+        -X POST \
+        -H 'x-apollo-operation-name: EnableSuperAdmin' \
+        -o /dev/null \
+        --data '[{"operationName":"EnableSuperAdmin","variables":{},"query":"mutation EnableSuperAdmin { enableSuperAdmin { user { id confluence { roles { isSuperAdmin }}}}}"}]' 
