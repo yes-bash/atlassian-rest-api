bfa1ddcf5f36fd70ff5ee8ca02ab91f7b6c67b64
yes-bash <123662544+yes-bash@users.noreply.github.com>
2024-08-09 18:55:54 +0200

    Create disable-super-admin.sh

---
+++ disable-super-admin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. .env
+
+curl -s --netrc-file ${netrcFile} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
+        -o /dev/null \
+        --url "${url}/cgraphql?q=EnableSuperAdmin" \
+        -X POST \
+        -H 'x-apollo-operation-name: DisableSuperAdmin' \
+        --data '[{"operationName":"DisableSuperAdmin","variables":{},"query":"mutation DisableSuperAdmin @asSuperAdmin { disableSuperAdmin { user { id confluence { roles { isSuperAdmin }}}}}"}]' |jq 
