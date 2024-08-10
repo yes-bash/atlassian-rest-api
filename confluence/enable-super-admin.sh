d6f0539c9813df2ffdecaf6a9d152408f415e19f
yes-bash <123662544+yes-bash@users.noreply.github.com>
2024-08-10 02:56:38 +0200

    fixes directory issues

--- confluence/enable-super-admin.sh
+++ confluence/enable-super-admin.sh
@@ -2,7 +2,7 @@
 
 . ../.env
 
-curl -s --netrc-file ${netrcFileConfluence} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
+curl -s --netrc-file ../${netrcFileConfluence} -s --header 'Accept: */*' --header 'Content-Type: application/json' \
         --url "${url}/cgraphql?q=EnableSuperAdmin" \
         -X POST \
         -H 'x-apollo-operation-name: EnableSuperAdmin' \
